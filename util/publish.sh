95cab28de1cb3e7bbdb53098a2c03b8f236c4938
Panic Stevenson <Panic.Stevenson@microsoft.com>
2020-06-23 18:17:27 -0700

    heh..

--- util/publish.sh
+++ util/publish.sh
@@ -7,9 +7,9 @@ OUT=$BASE/publish/cactbot-release/cactbot
 rm -rf "$OUT"
 mkdir -p "$OUT"
 
-cp -r "$BIN/{CactbotOverlay.dll,CactbotEventSource.dll}" "$OUT/"
-cp -r "$BASE/{ui/,resources/,*.md}" "$OUT/"
+cp -r "$BIN"/{CactbotOverlay.dll,CactbotEventSource.dll} "$OUT/"
+cp -r "$BASE"/{ui/,resources/,*.md} "$OUT/"
 
 # manually copy readme due to avoid local user/ files
 mkdir -p "$OUT/user/"
-cp "$BASE/user/{README.txt,*-example.*}" "$OUT/user/"
+cp "$BASE"/user/{README.txt,*-example.*} "$OUT/user/"
