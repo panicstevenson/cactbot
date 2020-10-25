6a808d955d5bb1a5605b276e43fc0d3fd57fcad2
Maiko Tan <maiko.tan.coding@gmail.com>
2020-10-26 01:16:16 +0800

    refactor: cpnvert to arrow function

--- resources/conditions.js
+++ resources/conditions.js
@@ -5,28 +5,18 @@
 
 export default class Conditions {
   static targetIsYou() {
-    return (data, matches) => {
-      return data.me == matches.target;
-    };
+    return (data, matches) => data.me == matches.target;
   }
   static targetIsNotYou() {
-    return (data, matches) => {
-      return data.me !== matches.target;
-    };
+    return (data, matches) => data.me !== matches.target;
   }
   static caresAboutAOE() {
-    return (data) => {
-      return data.role == 'tank' || data.role == 'healer' || data.CanAddle() || data.job == 'BLU';
-    };
+    return (data) => data.role == 'tank' || data.role == 'healer' || data.CanAddle() || data.job == 'BLU';
   }
   static caresAboutMagical() {
-    return (data) => {
-      return data.role == 'tank' || data.role == 'healer' || data.CanAddle() || data.job == 'BLU';
-    };
+    return (data) => data.role == 'tank' || data.role == 'healer' || data.CanAddle() || data.job == 'BLU';
   }
   static caresAboutPhysical() {
-    return (data) => {
-      return data.role == 'tank' || data.role == 'healer' || data.CanFeint() || data.job == 'BLU';
-    };
+    return (data) => data.role == 'tank' || data.role == 'healer' || data.CanFeint() || data.job == 'BLU';
   }
 }
